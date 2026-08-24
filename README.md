d6e696313318b273c3ec6a62cd3213eafd9e0902
perarasu1108 <perarasu1108@users.noreply.github.com>
2026-08-24 16:08:44 +0000

    leetie: Add 886. Possible Bipartition [Medium] (java)

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 > *Automatically synced by [leetie](https://github.com/leetie/leetie).*
 
-## Progress Summary: 1 Solved
+## Progress Summary: 2 Solved
 
 | Slug | Problem | Difficulty | Language | Problem Link | Solution Code |
 |------|---------|-----------|----------|--------------|---------------|
-| two-sum | Two Sum | Easy | java | [Problem](https://leetcode.com/problems/two-sum/) | [Solution](./solutions/Easy/0001-two-sum/solution.java) |
+| possible-bipartition | Possible Bipartition | Medium | java | [Problem](https://leetcode.com/problems/possible-bipartition/) | [Solution](./solutions/Medium/0886-possible-bipartition/solution.java) |
+| two-sum | 1. Two Sum | Easy | java | [Problem](https://leetcode.com/problems/two-sum/) | [Solution](./solutions/Easy/0001-two-sum/solution.java) |
---
+++ solutions/Medium/0886-possible-bipartition/solution.java
@@ -0,0 +1,46 @@
+// ──────────────────────────────────────────────────
+// Problem  : 886. Possible Bipartition
+// Difficulty: Medium
+// Tags     : Depth-First Search, Breadth-First Search, Union-Find, Graph Theory, Graph Coloring, Bipartite Graph
+// Link     : https://leetcode.com/problems/possible-bipartition/
+// Runtime  : 16 ms (beats 53%)
+// Memory   : 52828000 (beats 85%)
+// Language : java
+// Copyright: (c) 2026 perarasu1108. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] color = new int[n+1];
+        for(int i=0; i<=n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] temp: dislikes){
+            int from = temp[0];
+            int to = temp[1];
+            adj.get(from).add(to);
+            adj.get(to).add(from);
+        }
+        for(int i=1; i<=n; i++){
+            color[i] = -1;
+        }
+        for(int i=1; i<=n; i++){
+            if(color[i] == -1){
+                if(dfsPartition(i, dislikes, color, adj, 1) == false) return false;
+            }
+        }
+        return true;
+    }
+    private boolean dfsPartition(int node, int[][] dislikes, int[] color, List<List<Integer>> adj, int col){
+        color[node] = col;
+        for(int it: adj.get(node)){
+            if(color[it] == -1){
+                if(dfsPartition(it, dislikes, color, adj, 1-color[node]) == false) return false;
+            }
+            else if(color[it] == color[node]) return false;
+        }
+        return true;
+    }
+}
